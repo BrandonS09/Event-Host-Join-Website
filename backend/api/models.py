15820ba62f13d0af4cd12852c6ad774efc0a16dd
Brandon <brandonshen123@gmail.com>
2024-07-28 16:35:42 -0700

    Created basic event model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Event(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created']
+    
+    def __str__(self):
+        return self.name
